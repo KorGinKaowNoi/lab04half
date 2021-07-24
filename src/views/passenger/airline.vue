907ef1317899430355a0bf04b3511a0fd68ab3fe
GetThisDonut <thitisan_ch@elearning.cmu.ac.th>
2021-07-24 18:32:40 +0700

    final changes

--- src/views/passenger/airline.vue
+++ src/views/passenger/airline.vue
@@ -3,6 +3,8 @@
     <div v-else>from {{passenger.airline.country}}</div>
     <p v-if="isArray">and with slogan {{passenger.airline[0].slogan}}</p>
     <p v-else>and with slogan {{passenger.airline.slogan}}</p>
+    <img v-if="isArray" :src="passenger.airline[0].logo">
+    <img v-else :src="passenger.airline.logo">
 </template>
 <script>
 export default {
